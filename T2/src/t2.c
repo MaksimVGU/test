1710549907b51968dd9560606cd89771948e1332
DNS <DNS@DESKTOP-5R48CF7>
2022-05-18 19:18:05 +0300

    Test

--- T2/src/t2.c
+++ T2/src/t2.c
@@ -13,14 +13,82 @@
 // выбираем, продолжить ли работу
 #include <stdio.h>
 #include <stdlib.h>
-char type;
-char regul;
-double coordinate;
+typedef struct List_Vvod //Создаем структуру для данных ввода
+{
+    int type;
+    char deistvie;
+    double *peremenai_1;
+    double *peremenai_2;
+    char *prodolzit_li;
+    struct List_Vvod *next;
+}Vvod;
+typedef struct List_Vivod //Создаем структуру для данных вывода
+{
+    int rez;
+    struct List_Vivod *next;
+}Vivod;
+
+Vvod *create_node_Vvod(int set_type, char set_deistvie, double *set_peremenai_1, double *set_peremenai_2, char set_prodolzit_li) //Добавляем функцию для заполнения данных ввода
+{
+    Vvod *element=(Vvod *)malloc(sizeof(Vvod));
+    element -> type = set_type;
+    element -> deistvie = set_deistvie;
+    if (set_type>=2)
+    {
+        element -> peremenai_1 = set_peremenai_1;
+        element -> peremenai_2 = set_peremenai_2;
+    }
+    else
+    {
+        element -> peremenai_1 = set_peremenai_1;
+        if (set_deistvie!='!')
+        {
+            element -> peremenai_2 = set_peremenai_2;
+        }
+    }
+    element -> prodolzit_li = set_prodolzit_li;
+    element -> next = NULL;
+    return element;
+}
+
+void push_Vvod(Vvod **vvod_list, int set_type, char set_deistvie, double *set_peremenai_1, double *set_peremenai_2, char set_prodolzit_li) //добавляем "ячейки" для структуры ввода
+{
+    Vvod *new_element_vvod = create_node_Vvod(set_type, set_deistvie, set_peremenai_1,set_peremenai_2, set_prodolzit_li);
+    Vvod *tmp=*vvod_list;
+    while (tmp -> next != NULL)
+    {
+        tmp=tmp->next;
+    }
+    tmp->next = new_element_vvod;
+}
+
+Vivod *create_node_Vivod(float *set_rez) //Добавляем функцию для заполнения данных ввода
+{
+    Vivod *element=(Vivod *)malloc(sizeof(Vivod));
+    element -> rez = set_rez;
+    element -> next = NULL;
+    return element;
+}
+
+void push_Vivod(Vivod **vivod_list, float *set_rez)  //добавляем "ячейки" для структуры ввода
+{
+    Vvod *new_element_vivod = create_node_Vivod(set_rez);
+    Vvod *tmp=*vivod_list;
+    while (tmp -> next != NULL)
+    {
+        tmp=tmp->next;
+    }
+    tmp->next = new_element_vivod;
+}
+
 int main(int argc,char *argv[])
 {
 	setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
-	FILE *input,*output;
+    char regul;
+	Vvod *head, *current;
+    Vivod *head_result, *current_result;
+    FILE *input,*output;
 	char filename1[259];
 	char filename2[259];
 	printf("Введите откуда читать\n");
@@ -29,24 +97,17 @@ int main(int argc,char *argv[])
 	scanf("%s",filename2);
 	input=fopen(filename1,"r");
 	output=fopen(filename2,"w");
+
 	do
     {
-        //fprintf(output,"вектор(v)/Калькулятор(s)\n");
-        fscanf(input," %c", &type);
-        if (type=='s')
+        int size;
+        fscanf(input," %i", &size);
+        if (size==1)
         {
-            // пишем сначала все функции и их обозначения
-            //fprintf(output," сумма, знак +\n");
-        	//fprintf(output," разность, знак -\n");
-        	//fprintf(output," умножение, знак *\n");
-        	//fprintf(output," деление, знак /\n");
-        	//fprintf(output," степень, знак ^\n");
-        	//fprintf(output," факториал, знак !\n\n");
             double a,b,i,d; // задаем переменные типа double
-            char c; // задаем переменную типа char
-            //fprintf(output," введите операцию "); // выводим надпись с подсказкой
-            fscanf(input," %c", &c); //вводим с
-            switch(c) //блок функций
+            char deistvie; // задаем переменную типа char
+            fscanf(input," %c", &deistvie); //вводим с
+            switch(deistvie) //блок функций
             {
                 case '+'://блок суммы
                 	//fprintf(output,"введите первое число a=");
@@ -108,7 +169,7 @@ int main(int argc,char *argv[])
 
             }
         }
-        if(type=='v')//Блок векторов
+        if(size>=2)//Блок векторов
         {
         	int rez=0;
             int *t;
@@ -116,32 +177,23 @@ int main(int argc,char *argv[])
             int *b1;
             char deistvie;
             double a,b;
-            int size;
-            //fprintf(output,"введите размер вектора ");
-            fscanf(input,"%i",&size);
-            t=malloc(size*sizeof(int));
-            a1=malloc(size*sizeof(int));
-            b1=malloc(size*sizeof(int));
-            for(int i=0;i<size;i++)
-            {
-            	//fprintf(output,"введите координтаты вектора a ");
-                fscanf(input,"%lf",&a);
-                a1[i]=a;
-            }
-            for (int i = 0; i < size; i++)
-            {
-            	//fprintf(output,"введите координтаты вектора b ");
-                fscanf(input,"%lf",&b);
-                b1[i]=b;
-            }
-            //fprintf(output,"введите действие\n");
-            //fprintf(output,"Сложение вектора a и b --- '+'\n");
-            //fprintf(output,"вычитание из вектора a вектор b --- '-'\n");
-            //fprintf(output,"Скалярно умножить вектор a на b --- '*'\n");
             fscanf(input," %c",&deistvie);
             switch (deistvie)
             {
                 case '+': //блок сложения
+                    t=malloc(size*sizeof(int));
+                    a1=malloc(size*sizeof(int));
+                    b1=malloc(size*sizeof(int));
+                    for(int i=0;i<size;i++)
+                    {
+                        fscanf(input,"%lf",&a);
+                        a1[i]=a;
+                    }
+                    for (int i = 0; i < size; i++)
+                    {
+                        fscanf(input,"%lf",&b);
+                        b1[i]=b;
+                    }
                     for (int i = 0; i < size; i++)
                     {
                         t[i]=a1[i]+b1[i];
@@ -168,6 +220,19 @@ int main(int argc,char *argv[])
                     fprintf(output,")\n");
                 break;
                 case '-': //блок вычитания
+                    t=malloc(size*sizeof(int));
+                    a1=malloc(size*sizeof(int));
+                    b1=malloc(size*sizeof(int));
+                    for(int i=0;i<size;i++)
+                    {
+                        fscanf(input,"%lf",&a);
+                        a1[i]=a;
+                    }
+                    for (int i = 0; i < size; i++)
+                    {
+                        fscanf(input,"%lf",&b);
+                        b1[i]=b;
+                    }
                     for (int i = 0; i < size; i++)
                     {
                         t[i]=a1[i]-b1[i];
@@ -195,6 +260,19 @@ int main(int argc,char *argv[])
 
                 break;
                 case '*': //блок умножения скалярного
+                    t=malloc(size*sizeof(int));
+                    a1=malloc(size*sizeof(int));
+                    b1=malloc(size*sizeof(int));
+                    for(int i=0;i<size;i++)
+                    {
+                        fscanf(input,"%lf",&a);
+                        a1[i]=a;
+                    }
+                    for (int i = 0; i < size; i++)
+                    {
+                        fscanf(input,"%lf",&b);
+                        b1[i]=b;
+                    }
                     for (int i = 0; i < size; i++)
                     {
                         t[i]=a1[i]*b1[i];
