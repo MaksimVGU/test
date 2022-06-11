97043db1b28bc2ec8a68801f54f3e4d676dcb5da
DNS <DNS@DESKTOP-5R48CF7>
2022-06-11 13:44:21 +0300

    Final_queue

--- queue/src/queue21312.c
+++ queue/src/queue21312.c
@@ -24,8 +24,8 @@ typedef struct QUEUE_IN
     float *peremenai_2;
     struct QUEUE_IN *next_in;
 }QUEUE_IN;
-QUEUE_IN *head_in=NULL;
-QUEUE_IN *tail_in=NULL;
+QUEUE_IN *head_in_queue=NULL;
+QUEUE_IN *tail_in_queue=NULL;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////СТРУКТУРА ДЛЯ ВЫВОДА ДАННЫХ///////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -38,12 +38,12 @@ typedef struct QUEUE_OUT
     float *rez;
     struct QUEUE_OUT *next_out;
 }QUEUE_OUT;
-QUEUE_OUT *head_out=NULL;
-QUEUE_OUT *tail_out=NULL;
+QUEUE_OUT *head_out_queue=NULL;
+QUEUE_OUT *tail_out_queue=NULL;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ ПЕРЕХОДА НА СЛЕД ЭЛЕМЕНТ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-QUEUE_IN *next_el_in(QUEUE_IN *current)
+QUEUE_IN *next_el_in_queue(QUEUE_IN *current)
 {
     QUEUE_IN *next_el = current->next_in;
     return next_el;
@@ -51,7 +51,7 @@ QUEUE_IN *next_el_in(QUEUE_IN *current)
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ ПЕРЕХОДА НА СЛЕД ЭЛЕМЕНТ(OUT)///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-QUEUE_OUT *next_el_out(QUEUE_OUT *current)
+QUEUE_OUT *next_el_out_queue(QUEUE_OUT *current)
 {
     QUEUE_OUT *next_el = current->next_out;
     return next_el;
@@ -59,20 +59,20 @@ QUEUE_OUT *next_el_out(QUEUE_OUT *current)
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ УДАЛЕНЯИ ЭЛЕМЕНТА///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-QUEUE_IN *del_in(QUEUE_IN *current)
+QUEUE_IN *del_in_queue(QUEUE_IN *current)
 {
     QUEUE_IN *del=current;
-    current=next_el_in(current);
+    current=next_el_in_queue(current);
     free(del);
     return current;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ УДАЛЕНЯИ ЭЛЕМЕНТА(OUT)//////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-QUEUE_OUT *del_out(QUEUE_OUT *current)
+QUEUE_OUT *del_out_queue(QUEUE_OUT *current)
 {
     QUEUE_OUT *del=current;
-    current=current->next_out;
+    current=next_el_out_queue(current);
     free(del);
     return current;
 }
@@ -164,7 +164,7 @@ float*vector(char deistvie,int size, float *peremenai_1,float *peremenai_2)
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ ДЛЯ ДОБАВЛЕНИЯ ЭЛЕМЕНТА ВВОДА///////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void add_el_in(float *p_1, char d, float *p_2, int s)
+void add_el_in_queue(float *p_1, char d, float *p_2, int s)
 {
 		QUEUE_IN *tmp=malloc(1*sizeof(QUEUE_IN));
 		tmp->deistvie=d;
@@ -180,21 +180,21 @@ void add_el_in(float *p_1, char d, float *p_2, int s)
 			tmp->peremenai_2=NULL;
 		}
 		tmp->next_in=NULL;
-		if (tail_in != NULL)
+		if (tail_in_queue != NULL)
 		{
-			tail_in->next_in=tmp;
-			tail_in=tmp;
+			tail_in_queue->next_in=tmp;
+			tail_in_queue=tmp;
 		}
 		else
 		{
-			head_in=tmp;
-			tail_in=tmp;
+			head_in_queue=tmp;
+			tail_in_queue=tmp;
 		}
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////ФУНКЦИЯ ДЛЯ ДОБАВЛЕНИЯ ЭЛЕМЕНТА ВЫВОДА//////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void add_el_out(QUEUE_OUT *head_OUT, QUEUE_IN *head_in)
+void add_el_out_queue(QUEUE_OUT *head_OUT, QUEUE_IN *head_in)
 {
     QUEUE_OUT *tmp_res=malloc(1*sizeof(QUEUE_OUT));
     if (head_in->size>1)
@@ -215,15 +215,15 @@ void add_el_out(QUEUE_OUT *head_OUT, QUEUE_IN *head_in)
         tmp_res->rez=num(head_in->deistvie,head_in->peremenai_1,head_in->peremenai_2);
     }
     tmp_res->next_out=NULL;
-    if (tail_out != NULL)
+    if (tail_out_queue != NULL)
     {
-        tail_out->next_out=tmp_res;
-        tail_out=tmp_res;
+        tail_out_queue->next_out=tmp_res;
+        tail_out_queue=tmp_res;
     }
     else
     {
-        head_out=tmp_res;
-        tail_out=tmp_res;
+        head_out_queue=tmp_res;
+        tail_out_queue=tmp_res;
     }
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -273,64 +273,66 @@ int main(int argc,char *argv[])
             			fscanf(input," %f",&p_1[i]);
             		}
             	}
-                add_el_in(p_1, d , p_2 , s);
+                add_el_in_queue(p_1, d , p_2 , s);
             }
             fclose(input);
             output=fopen(File_OUT,"w");
-            while (head_in != NULL)
+            while (head_in_queue != NULL)
             {
-            	add_el_out(head_out,head_in);
-                head_in=del_in(head_in);
+            	add_el_out_queue(head_out_queue,head_in_queue);
+                head_in_queue=del_in_queue(head_in_queue);
             }
-            while (head_out!=NULL)
+            while (head_out_queue!=NULL)
             {
-                if (head_out->size>1)
+                if (head_out_queue->size>1)
                 {
                     fprintf(output," ( ");
-                    for (int i=0;i<head_out->size;i++)
+                    for (int i=0;i<head_out_queue->size;i++)
                     {
-                        fprintf(output," %1.f ", head_out->peremenai_1[i]);
+                        fprintf(output," %1.f ", head_out_queue->peremenai_1[i]);
                     }
                     fprintf(output," ) ");
-                    fprintf(output,"%c",head_out->deistvie);
+                    fprintf(output,"%c",head_out_queue->deistvie);
                     fprintf(output," ( ");
-                    for (int i=0;i<head_out->size;i++)
+                    for (int i=0;i<head_out_queue->size;i++)
                     {
-                        fprintf(output," %1.f ", head_out->peremenai_2[i]);
+                        fprintf(output," %1.f ", head_out_queue->peremenai_2[i]);
                     }
                     fprintf(output," ) ");
                     fprintf(output," = ");
                     fprintf(output," ( ");
-                    if(head_out->deistvie != '*')
+                    if(head_out_queue->deistvie != '*')
                     {
-                    	for (int i=0;i<head_out->size;i++)
+                    	for (int i=0;i<head_out_queue->size;i++)
                     	{
-                    		fprintf(output," %0.1f ", head_out->rez[i]);
+                    		fprintf(output," %0.1f ", head_out_queue->rez[i]);
                     	}
                     }
                     else
                     {
-                    	fprintf(output," %0.1f ", head_out->rez[0]);
+                    	fprintf(output," %0.1f ", head_out_queue->rez[0]);
                     }
 
                     fprintf(output," )\n");
                 }
-                else if (head_out->size==1)
+                else if (head_out_queue->size==1)
                 {
-                    if(head_out->deistvie != '!')
+                    if(head_out_queue->deistvie != '!')
                     {
-                        fprintf(output," %0.1f %c %f = %0.1f \n",head_out->peremenai_1[0],head_out->deistvie,head_out->peremenai_2[0],head_out->rez[0]);
+                        fprintf(output," %0.1f %c %0.1f = %0.1f \n",head_out_queue->peremenai_1[0],head_out_queue->deistvie,head_out_queue->peremenai_2[0],head_out_queue->rez[0]);
                     }
                     else
                     {
-                        fprintf(output," %0.1f %c = %0.1f \n",head_out->peremenai_1[0],head_out->deistvie,head_out->rez[0]);
+                        fprintf(output," %0.1f %c = %0.1f \n",head_out_queue->peremenai_1[0],head_out_queue->deistvie,head_out_queue->rez[0]);
                     }
                 }
-                head_out=del_out(head_out);
+                head_out_queue=del_out_queue(head_out_queue);
             }
             fclose(output);
-            tail_in=NULL;
-            tail_out=NULL;
+            tail_in_queue=NULL;
+            tail_out_queue=NULL;
+            head_in_queue=NULL;
+            head_out_queue=NULL;
             free(p_1);
             free(p_2);
         }
